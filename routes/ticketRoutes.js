ede24f20873482d4c6bc4cc29e8c2e405da6fb1d
pragathyT <pragathyt95@gmail.com>
2026-09-02 06:52:22 +0530

    Removed QR related code - partially working

--- routes/ticketRoutes.js
+++ routes/ticketRoutes.js
@@ -2,14 +2,12 @@ const express = require("express");
 
 const router = express.Router();
 
-const {
-  generateTicketQR,
-  downloadTicket
-} = require("../controllers/ticketController");
+const { downloadTicket } = require("../controllers/ticketController");
+// const { generateTicketQR } = require("../controllers/ticketController");
 
 const { protect } = require("../middleware/authMiddleware");
 
-router.get("/:id/qr", protect, generateTicketQR);
+// router.get("/:id/qr", protect, generateTicketQR);
 router.get("/:id/download", protect, downloadTicket);
 
 module.exports = router;
